6aaea266877e1570f6c4cd30e0770e0d9adea19b
Hardika Laiya <hardika181189@gmail.com>
2025-07-07 16:25:19 +0530

    Array and ArrayList

---
+++ Array_ArrayLsit.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Array_ArrayLsit{
+    public static void main(String[] args){
+        int arr[] = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        ArrayList<Integer> al = new ArrayList<Integer>();
+            al.add(1);
+            al.add(2);
+            al.add(3);
+            al.add(4);
+            al.add(5);
+            
+            System.out.println(al);
+            System.out.println(Arrays.toString(arr));
+            
+    }
+}
