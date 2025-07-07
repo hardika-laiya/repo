f217c92eb514652b64a683a5088ad1ded4735de5
Hardika Laiya <hardika181189@gmail.com>
2025-07-07 16:38:17 +0530

    changed value

--- Array_ArrayLsit.java
+++ Array_ArrayLsit.java
@@ -10,7 +10,7 @@ public class Array_ArrayLsit{
             al.add(2);
             al.add(3);
             al.add(4);
-            al.add(5);
+            al.add(25);
             
             System.out.println(al);
             System.out.println(Arrays.toString(arr));
